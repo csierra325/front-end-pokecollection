fe0ea27dccb37dcfda427e216bbd0c7575e2fee2
Camille <camillemsierra@gmail.com>
2020-09-08 19:39:22 -0700

    reorganize sort

--- src/components/newTrainer/newTrainer.js
+++ src/components/newTrainer/newTrainer.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import {
-    useHistory,
-    useLocation
-  } from "react-router-dom";
+  useHistory,
+  useLocation
+} from "react-router-dom";
 import bg from '../../images/bg.jpg';
 import './newTrainer.css';
-  
-  function LoginPage() {
-    let history = useHistory();
-    let location = useLocation();
-  
-    let { from } = location.state || { from: { pathname: "/success" } };
-    
-    let login = () => {
-        history.replace(from);
-    };
-  
-    return (
-        <>
-        <div  className="bg" style={{ backgroundImage: `url(${bg})`}}>
-            <button onClick={login}>Log in</button>
-        </div>
-        
-      </>
-    );
-  }
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/success" } };
+
+  let login = () => {
+    history.replace(from);
+  };
+
+  return (
+    <>
+      <div className="bg" style={{ backgroundImage: `url(${bg})` }}>
+        <button onClick={login}>Log in</button>
+      </div>
+
+    </>
+  );
+}
 export default LoginPage;
--- src/components/trainer/trainer.js
+++ src/components/trainer/trainer.js
@@ -6,284 +6,242 @@ const localStorageEnum = {
     trainerId: 'trainerId',
 }
 class Trainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            trainerId: '',
-            newTrainer: '',
-            newTrainerId: '',
-            newTrainerCurrency: 0,
-            pokeCollectionId: '',
-            pokemonSpirite: [],
-            pokemonName: [],
-            pokemonId: [],
-            pokemonRarity: [],
-            error: '',
-            currency: 0,
-            sortedPokemon: [],
-            data: '',
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
 
-    componentDidMount = async () => {
-        debugger;
-        if (this.state.trainerId === '') {
-            debugger
-            const trainerId = localStorage.getItem(localStorageEnum.trainerId);
-            const res = await fetch(`http://localhost:3000/trainer/${trainerId}`);
-            if (!res.ok) {
-                const error = await res.text();
-                return console.log(error);
-            }
-            const data = await res.json();
-            if (data) {
-                console.log(data.pokecollection.pokemons);
-                data.pokecollection.pokemons.forEach((pokemon) => {
-                    this.setState(() => ({
-                        newTrainer: data.name,
-                        newTrainerId: data._id,
-                        newTrainerCurrency: data.currency,
-                        pokeCollectionId: data.pokecollection,
-                        pokemonSpirite: [...this.state.pokemonSpirite, pokemon.sprite],
-                        pokemonName: [...this.state.pokemonName, pokemon.name],
-                        data: data.pokecollection.pokemons,
-                    }));
-                });
-            }
-        }
-    }
-
-    postTrainer = async () => {
-        const message = {
-            name: `${this.state.name}`,
-        };
-        await fetch('http://localhost:3000/trainer', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            },
-            body: JSON.stringify(message),
-        })
-            .then((res) => {
-                if (!res.ok) throw Error(res.statusText);
-                return res.json();
-            })
-            .then(async (res) => {
-                this.setState(() => ({
-                    newTrainer: res.name,
-                    newTrainerId: res._id,
-                    newTrainerCurrency: res.currency,
-                    pokeCollectionId: res.pokecollection,
-
-                }));
-                localStorage.setItem(localStorageEnum.trainerId, res._id);
-            });
-    }
-
-    buyBackPokemonPack = async () => {
-        const newPack = {
-            trainerId: `${this.state.newTrainerId}`,
-            packType: 'basic',
-        };
-        await fetch('http://localhost:3000/pokeCollection/pack', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            },
-            body: JSON.stringify(newPack),
-        })
-            .then((res) => {
-                if (!res.ok) {
-                    this.setState(() => ({
-                        error: 'Sorry, you do not have enough money.',
-                    }));
-                }
-                return res.json();
-            })
-            .then(async (res) => {
-                res.map((basicPokemon) => this.setState(() => ({
-                    pokemonSpirite: [...this.state.pokemonSpirite, basicPokemon.sprite],
-                })));
-            });
-    }
-
-    buyPremiumPokemonPack = async () => {
-        const newPack = {
-            trainerId: `${this.state.newTrainerId}`,
-            packType: 'premium',
-        };
-        await fetch('http://localhost:3000/pokeCollection/pack', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            },
-            body: JSON.stringify(newPack),
-        })
-            .then((res) => {
-                if (!res.ok) {
-                    this.setState(() => ({
-                        error: 'Sorry, you do not have enough money.',
-                    }));
-                }
-                return res.json();
-            })
-            .then(async (res) => {
-                res.map((premiumPokemon) => this.setState(() => ({
-                    pokemonSpirite: [...this.state.pokemonSpirite, premiumPokemon.sprite],
-                })));
-            });
-    }
-
-    getCurrency = (e) => {
-        const currency = e.target.value;
+  componentDidMount = async () => {
+    const trainerId = localStorage.getItem(localStorageEnum.trainerId);
+    if (trainerId) {
+      const res = await fetch(`http://localhost:3000/trainer/${trainerId}`);
+      if (!res.ok) {
+        const error = await res.text();
+        return console.log(error);
+      }
+      const data = await res.json();
+      if (data) {
         this.setState(() => ({
-            currency,
+          newTrainer: data.name,
+          newTrainerId: data._id,
+          newTrainerCurrency: data.currency,
+          pokeCollection: data.pokecollection,
         }));
+      }
     }
+  }
 
-    handleNameChange = (e) => {
-        const name = e.target.value;
-        this.setState(() => ({
-            name,
-        }));
+  postTrainer = async () => {
+    const message = {
+      name: `${this.state.name}`,
+    };
+    const res = await fetch('http://localhost:3000/trainer', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
+    if (!res.ok) throw Error(res.statusText);
+    const data = await res.json();
+    this.setState(() => ({
+      newTrainer: data.name,
+      newTrainerId: data._id,
+      newTrainerCurrency: data.currency,
+      pokeCollection: data.pokecollection,
+    }));
+    localStorage.setItem(localStorageEnum.trainerId, data._id);
+  }
+
+  buyBackPokemonPack = async () => {
+      const newPack = {
+          trainerId: `${this.state.newTrainerId}`,
+          packType: 'basic',
+      };
+      let res = await fetch('http://localhost:3000/pokeCollection/pack', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+          },
+          body: JSON.stringify(newPack),
+      })
+      if (!res.ok) {
+          return this.setState({ error: 'Sorry, you do not have enough money.' });
+      }
+      const data = await res.json();
+      this.setState({
+          pokeCollection: {
+              ...this.state.pokeCollection,
+              pokemons: [...this.state.pokeCollection.pokemons, ...data]
+          }
+      });
+  }
+
+  // redo this
+  buyPremiumPokemonPack = async () => {
+      const newPack = {
+          trainerId: `${this.state.newTrainerId}`,
+          packType: 'premium',
+      };
+      await fetch('http://localhost:3000/pokeCollection/pack', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+          },
+          body: JSON.stringify(newPack),
+      })
+          .then((res) => {
+              if (!res.ok) {
+                  this.setState(() => ({
+                      error: 'Sorry, you do not have enough money.',
+                  }));
+              }
+              return res.json();
+          })
+          .then(async (res) => {
+              res.map((premiumPokemon) => this.setState(() => ({
+                  pokemonSpirite: [...this.state.pokemonSpirite, premiumPokemon.sprite],
+              })));
+          });
+  }
+
+  getCurrency = (e) => {
+      const currency = e.target.value;
+      this.setState(() => ({
+          currency,
+      }));
+  }
+
+  handleNameChange = (e) => {
+      const name = e.target.value;
+      this.setState(() => ({
+          name,
+      }));
+  }
+
+  handleIdChange = (e) => {
+      const trainerId = e.target.value;
+      this.setState(() => ({
+          trainerId,
+      }));
+  }
+
+  // redo all the sorts
+  ascendingSort = () => {
+    const compareName = (a, b) => {
+      return (a['name'].toLowerCase() > b['name'].toLowerCase()) ? 1 : (a['name'].toLowerCase() < b['name'].toLowerCase()) ? -1 : 0;
     }
+    const sort = this.state.pokeCollection.pokemons.sort(compareName)
+    this.setState(() => ({
+      pokeCollection: {
+        pokemons: sort
+      }
+    }))
+  }
 
-    handleIdChange = (e) => {
-        const trainerId = e.target.value;
-        this.setState(() => ({
-            trainerId,
-        }));
+  descendingSort = () => {
+    const compareName = (b, a) => {
+      return (a['name'].toLowerCase() > b['name'].toLowerCase()) ? 1 : (a['name'].toLowerCase() < b['name'].toLowerCase()) ? -1 : 0;
     }
+    const sort = this.state.pokeCollection.pokemons.sort(compareName)
+    this.setState(() => ({
+      pokeCollection: {
+        pokemons: sort
+      }
+    }))
+  }
 
-    ascendingSort = () => {
-        const compareNameAZ = (a, b) => {
-            return (a['name'].toLowerCase() > b['name'].toLowerCase()) ? 1 : (a['name'].toLowerCase() < b['name'].toLowerCase()) ? -1 : 0;
-        }
-        const sort = this.state.data.sort(compareNameAZ)
-        const sortedSpirte = [];
-        for (var i = 0; i < sort.length; i++) {
-            sortedSpirte.push(sort[i].sprite)
-        }
-        this.setState(() => ({
-            pokemonSpirite: sortedSpirte
-        }))
-    }
+  idSort = () => {
+      const compareName = (a, b) => {
+        return (a['_id'] > b['_id']) ? 1 : (a['_id'] < b['_id']) ? -1 : 0;
+      }
+    const sort = this.state.pokeCollection.pokemons.sort(compareName)
+    this.setState(() => ({
+      pokeCollection: {
+        pokemons: sort
+      }
+    }))
+  }
 
-    descendingSort = () => {
-        const compareNameAZ = (b, a) => {
-            return (a['name'].toLowerCase() > b['name'].toLowerCase()) ? 1 : (a['name'].toLowerCase() < b['name'].toLowerCase()) ? -1 : 0;
+  raritySort = () => {
+      const compareName = (a, b) => {
+        return (a['rarity'] > b['rarity']) ? 1 : (a['rarity'] < b['rarity']) ? -1 : 0;
+      }
+      const sort = this.state.pokeCollection.pokemons.sort(compareName)
+      this.setState(() => ({
+        pokeCollection: {
+          pokemons: sort
         }
-        const sortedSpirte = [];
-        const sort = this.state.data.sort(compareNameAZ)
-        for (var i = 0; i < sort.length; i++) {
-            sortedSpirte.push(sort[i].sprite)
-        }
-        this.setState(() => ({
-            pokemonSpirite: sortedSpirte
-        }))
-    }
+      }))
+  }
 
-    idSort = () => {
-        const compareNameAZ = (a, b) => {
-            return (a['_id'] > b['_id']) ? 1 : (a['_id'] < b['_id']) ? -1 : 0;
-        }
-        const sort = this.state.data.sort(compareNameAZ)
-        const sortedSpirte = [];
-        for (var i = 0; i < sort.length; i++) {
-            sortedSpirte.push(sort[i].sprite)
-        }
-        this.setState(() => ({
-            pokemonSpirite: sortedSpirte
-        }))
-    }
-
-    raritySort = () => {
-        const compareNameAZ = (a, b) => {
-            return (a['rarity'] > b['rarity']) ? 1 : (a['rarity'] < b['rarity']) ? -1 : 0;
-        }
-        const sort = this.state.data.sort(compareNameAZ)
-        const sortedSpirte = [];
-        for (var i = 0; i < sort.length; i++) {
-            sortedSpirte.push(sort[i].sprite)
-        }
-        this.setState(() => ({
-            pokemonSpirite: sortedSpirte
-        }))
-    }
-
-    render() {
-        return (
-            <>
-                <div className="bg" style={{ backgroundImage: `url(${bg})` }}></div>
-                <div className="trainer-container">
-                    <div className="trainer-group">
-                        {!this.state.newTrainer &&
-                            <div>
-                                <div className="trainer-header">Welcome to the Pokemon Game</div>
-                                <div className="trainer-subheader">Enter your name to begin</div>
-                                <div className="trainer-subheader">
-                                    <input
-                                        className="trainer-input"
-                                        value={this.state.name}
-                                        onChange={this.handleNameChange}
-                                        type="text"
-                                    />
-                                    <div className="trainer-button" onClick={this.postTrainer}>Submit</div>
-                                </div>
-                            </div>
-                        }
-                        {this.state.newTrainer &&
-                            <div className="trainer-results">
-                                <div className="error">{this.state.error}</div>
-                                <div className="welcome">Welcome {this.state.newTrainer}!</div>
-                                <div>Currency: ${this.state.newTrainerCurrency}</div>
-                                <div>Trainer ID: {this.state.newTrainerId}</div>
-                                <div className="pack-buttons">
-                                    <div className="premium-button" onClick={this.buyPremiumPokemonPack}>Buy Premium Pokemon Pack</div>
-                                    <div className="basic-button" onClick={this.buyBackPokemonPack}>Buy Basic Pokemon Pack</div>
-                                </div>
-                                <select onChange={(e) => {
-                                    const value = e.target.value;
-                                    if (value === 'az') {
-                                        this.ascendingSort();
-                                    }
-                                    if (value === 'za') {
-                                        this.descendingSort();
-                                    }
-                                    if (value === 'id') {
-                                        this.idSort();
-                                    }
-                                    if (value === 'rarity') {
-                                        this.raritySort()
-                                    }
-                                }} name="sort" id="sort">
-                                    <option value="">Sort</option>
-                                    <option value="az">A-Z</option>
-                                    <option value="za">Z-A</option>
-                                    <option value="id">ID</option>
-                                    <option value="rarity">Rarity</option>
-                                </select>
-                                {/* <input
-                                value={this.state.currency}
-                                onChange={this.getCurrency}
-                            /> */}
-                                {/* <button onClick={this.addCurrency}>Add Currency</button> */}
-                                <div>
-                                    <div>Pokemon Collection</div>
-                                    {this.state.pokemonSpirite.map((spirte) => <img alt="pokemon" src={spirte} />)}
-                                    {this.state.sortedPokemon.map((pokemon) => <div>{pokemon.name}</div>)}
-                                </div>
-                            </div>
-                        }
-                    </div>
+  render() {
+    return (
+      <>
+        <div className="bg" style={{ backgroundImage: `url(${bg})` }}></div>
+        <div className="trainer-container">
+          <div className="trainer-group">
+            {!this.state.newTrainer &&
+              <div>
+                <div className="trainer-header">Welcome to the Pokemon Game</div>
+                <div className="trainer-subheader">Enter your name to begin</div>
+                <div className="trainer-subheader">
+                  <input
+                    className="trainer-input"
+                    value={this.state.name}
+                    onChange={this.handleNameChange}
+                    type="text"
+                  />
+                  <div className="trainer-button" onClick={this.postTrainer}>Submit</div>
                 </div>
-            </>
-        );
-    }
+              </div>
+            }
+            {this.state.newTrainer &&
+              <div className="trainer-results">
+                <div className="error">{this.state.error}</div>
+                <div className="welcome">Welcome {this.state.newTrainer}!</div>
+                <div>Currency: ${this.state.newTrainerCurrency}</div>
+                <div>Trainer ID: {this.state.newTrainerId}</div>
+                <div className="pack-buttons">
+                  <div className="premium-button" onClick={this.buyPremiumPokemonPack}>Buy Premium Pokemon Pack</div>
+                  <div className="basic-button" onClick={this.buyBackPokemonPack}>Buy Basic Pokemon Pack</div>
+                </div>
+                <select onChange={(e) => {
+                  const value = e.target.value;
+                  if (value === 'az') {
+                      this.ascendingSort();
+                  }
+                  if (value === 'za') {
+                      this.descendingSort();
+                  }
+                  if (value === 'id') {
+                      this.idSort();
+                  }
+                  if (value === 'rarity') {
+                      this.raritySort()
+                  }
+                }} name="sort" id="sort">
+                  <option value="">Sort</option>
+                  <option value="az">A-Z</option>
+                  <option value="za">Z-A</option>
+                  <option value="id">ID</option>
+                  <option value="rarity">Rarity</option>
+                </select>
+                <div>
+                  <div>Pokemon Collection</div>
+                  {/* add names and numbers etc */}
+                  {this.state.pokeCollection.pokemons.map(pokemon => <img alt="pokemon" src={pokemon.sprite} />)}
+                </div>
+              </div>
+            }
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Trainer;
